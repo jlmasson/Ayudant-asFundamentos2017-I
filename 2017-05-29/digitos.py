ec19a76cd624fd99bbac4d107d077efab49ab191
José Luis Massón <jlmasson@espol.edu.ec>
2017-05-31 01:36:10 -0500

    CÃdigo comentado del dÃa lunes

--- 2017-05-29/digitos.py
+++ 2017-05-29/digitos.py
@@ -1,12 +1,25 @@
-n = int(input("Ingrese valor: "))
+# Dado un número:
+# Calcular la cantidad de dígitos que tiene y la respectiva suma de estos
 
+n = int(input("Ingrese valor: ")) # Pido el numero
+
+# Tengo variables de dos tipos:
+# Acumuladora -> sumaDigitos
+# Contadora -> totalDigitos
+
+# Ambas están inicializas en 0, porque antes de realizar todo el procedimiento
+# no se ha sumado ningún digito, y tampoco se ha contado ningún número.
 totalDigitos = sumaDigitos = 0
 
+# Yo puedo obtener los dígitos si el cociente es mayor a 0
 while n > 0: # n != 0
-	d = n % 10
-	sumaDigitos += d
-	totalDigitos += 1
-	n //= 10
+	
+	d = n % 10 # Antes de dividir obtengo el dígito
+
+	sumaDigitos += d # Sumo el dígito
+
+	totalDigitos += 1 # Cuento el dígito. Nótese que es la cantidad de divisiones
+	n //= 10 # Actualizo el número
 
 print("La suma de los digitos es {}".format(sumaDigitos))
 print("El total de dígitos es {}".format(totalDigitos))
--- 2017-05-29/esKaprekar.py
+++ 2017-05-29/esKaprekar.py
@@ -1,24 +1,34 @@
-n = int(input("Ingrese número: "))
+n = int(input("Ingrese número: ")) # Pido el número
 
-aux = n
+cuadrado = n ** 2 # Elevo el numero al cuadrado
 
-cuadrado = n ** 2
+suma = 0 # Inicio la variable suma en 0
 
-suma = 0
+i = 1	# Inicio la variable i (exponente) en 1
 
-i = 1
+aux = cuadrado # Creo una variable auxiliar para dividir de manera sucesiva.
 
+# Esta es la condición de continuación del lazo
+# obtenidos a partir de la compresión de los 3 ejemplos mostrados
 while (aux > 0 and suma != n):
 
+	# Cada vez que se divide, el divisor se multiplica por 10
 	divisor = 10 ** i
+
+	# Obtengo el cociente y residuo
 	c = cuadrado // divisor
 	r = cuadrado % divisor
 
 	suma = c + r
 	i += 1
+
+	# Actualizo el auxiliar
+	# En el 3er ejemplo se aprecia que el lazo se detiene
+	# porque el cociente da como resultado 0
 	aux //= 10
 
-if (aux == 0 and suma == n):
+# Pregunto si la suma al final es igual al numero n ingresado
+if (suma == n):
 	print("El numero es Kaprekar")
 else:
 	print("El numero no es Kaprekar")
