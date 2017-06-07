851ee5b6f54fea5bd732baa45042ab05e34cb8c4
Made Velasco <m_velasco93@hotmail.com>
2017-06-07 00:21:24 -0500

    Descripcion al miniongame

--- Unidad 4 - Listas/theminiongame.py
+++ Unidad 4 - Listas/theminiongame.py
@@ -1,3 +1,13 @@
+#
+#Autor: Madelyne Velasco
+#
+
+"""
+A ambos jugadores se les da una string de tamaño S y ambos tienen que hacer substrings usando las letras de S (Estas substrings son consecutivas)
+Stuart tiene que hacer substrings que empiecen con consonantes mientras que Kevin tiene que hacer substrings que empiecen con vocales
+Cada jugador gana un punto por cada substring. Tu trabajo es determinar quien gana el juego.
+"""
+
 intro = input ('Ingrese una palabra: ')
 
 stuart = 0
@@ -9,7 +19,7 @@ long = len(intro)
 
 for i in range(0,long):
 	for x in range(i+1):
-		if intro[x] in vowels:
+		if intro[i] in vowels:
 			slc = intro[i:x]
 			kevin = kevin + 1
 		else:
